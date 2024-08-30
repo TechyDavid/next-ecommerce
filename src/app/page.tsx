f7f6a78e225e72160f7c478e1857438d2402cb9a
TechyDavid <drufus015@gmail.com>
2024-08-30 09:55:32 +0100

    hmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { RippleDemo } from '@/components/example/rippledemo';
+
 
 export default function Home() {
   return (
     <main >
-      <div className='w-full h-full'>
-       <RippleDemo/>
+      <div className='w-full h-full  bg-slate-200'>
+  
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,45 +8,7 @@ module.exports = {
         fontFamily: {
           monument: ['Monument', 'sans-serif'],
         },
-        animation: {
-          ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
-        },
-        keyframes: {
-          ripple: {
-            "0%, 100%": {
-              transform: "translate(-50%, -50%) scale(1)",
-            },
-            "50%": {
-              transform: "translate(-50%, -50%) scale(0.9)",
-            },
-          },
-        },
-        animation: {
-          "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
-          slide: "slide var(--speed) ease-in-out infinite alternate",
-        },
-        keyframes: {
-          "spin-around": {
-            "0%": {
-              transform: "translateZ(0) rotate(0)",
-            },
-            "15%, 35%": {
-              transform: "translateZ(0) rotate(90deg)",
-            },
-            "65%, 85%": {
-              transform: "translateZ(0) rotate(270deg)",
-            },
-            "100%": {
-              transform: "translateZ(0) rotate(360deg)",
-            },
-          },
-          slide: {
-            to: {
-              transform: "translate(calc(100cqw - 100%), 0)",
-            },
-          },
-        },
-      },
+       },
     },
     plugins: [],
   }
