866b06bd38297b0dc87fadf82558fe196e72ed4d
TechyDavid <drufus015@gmail.com>
2024-09-01 19:50:46 +0100

    heyy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className="">Navbar</div>
+        <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64">Navbar</div>
     )
 }
 export default Navbar
