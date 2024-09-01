46bbd391057b207da5afbb94a71fbecdea520b65
TechyDavid <drufus015@gmail.com>
2024-09-01 19:58:54 +0100

    link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link"
 const Navbar = () => {
     return (
-        <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64">Navbar</div>
+        <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64">
+            {/* {Mobile} */}
+            <link rel="stylesheet" href="" >Rufus</link>
+        </div>
     )
 }
 export default Navbar
