24db0f92ebd7fe54ee5dd9e9dd2e1e09c5b58e77
TechyDavid <drufus015@gmail.com>
2024-09-03 17:42:07 +0100

    hh

---
+++ src/components/Menu.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+const Menu = () => {
+
+    const [open,setOpen] = useState(false)
+
+
+    return (
+        <div className="">Menu</div>
+    )
+}
+export default Menu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import Menu from "./Menu"
 const Navbar = () => {
     return (
         <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64">
             {/* {Mobile} */}
             <link rel="stylesheet" href="" >Rufus</link>
+            <Menu/>
         </div>
     )
 }
