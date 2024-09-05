43e4adb7a37436a9a050836dbf3226c82abbf2de
TechyDavid <drufus015@gmail.com>
2024-09-05 21:37:23 +0100

    hmm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,15 @@ import Menu from "./Menu"
 const Navbar = () => {
     return (
         <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:64">
-            {/* {Mobile} */}
-            <Link href="/rufus">Rufus</Link>
-            <Menu/>
+            <div className="h-full flex items-center justify-between">
+                {/* {Mobile} */}
+                <Link href="/rufus">
+                <div className="text-2xl tracking-wide">
+                Rufus
+                </div>
+                </Link>
+                <Menu/>
+            </div>
         </div>
     )
 }
