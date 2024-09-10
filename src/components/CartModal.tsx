3d25cbdf30579c7bd219bc7d791232ff58fe25c7
TechyDavid <drufus015@gmail.com>
2024-09-10 12:00:15 +0100

    sewa

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -3,7 +3,7 @@
 
 const CartModal = () => {
     return (
-        <div>Cartmodal</div>
+        <div className="absolute p-4 rounded-md text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">Cartmodal</div>
     )
 }
 
