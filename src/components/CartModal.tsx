ddbb553fbb2c48358d5e109599a98e45bb119b60
TechyDavid <drufus015@gmail.com>
2024-09-10 12:34:29 +0100

    mehnn

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -2,8 +2,19 @@
 
 
 const CartModal = () => {
+
+    const cartItems = false
+
     return (
-        <div className="absolute p-4 rounded-md text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">Cartmodal</div>
+        <div className="absolute p-4 rounded-md text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+              {!cartItems ? (
+            <div className="">Cart is empty</div>
+        ) : (
+            <div className=""></div>
+        )}
+
+        </div>
+      
     )
 }
 
