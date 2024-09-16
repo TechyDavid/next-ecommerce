de203df96894bd3a7e6b25a12d73a24b332ffebf
TechyDavid <drufus015@gmail.com>
2024-09-16 19:09:28 +0100

    hh

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,25 +1,41 @@
-"use client"
+"use client";
 
-import { url } from "inspector"
-import { title } from "process"
+import { useState } from "react";
 
-const slides = {
-    {
-        id: 1,
-        title: "Summmer Sale Sollections",
-        description: "Sale! Up to 50% off!",
-        img: "",
-        url: "/",
-        bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
-    }
-}
+const slides = [
+  {
+    id: 1,
+    title: "Summer Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Winter Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+  },
+  {
+    id: 3,
+    title: "Spring Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+  },
+];
 
-const Homepage = () => {
-    return (
-        <div>
+const Slider = () => {
+  const [current, setCurrent] = useState(0);
+  return (
+    <div className="">
+      Slider
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Homepage
+export default Slider;
