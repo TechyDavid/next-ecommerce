075948fb66f2436ec07562f8adb5cf126c239840
TechyDavid <drufus015@gmail.com>
2024-09-18 19:50:38 +0100

    fff

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -34,23 +34,32 @@ const slides = [
 const Slider = () => {
   const [current, setCurrent] = useState(0);
   return (
-    <div className="h-[]calc(100vh-80px) overflow-hidden">
+    <div className="h-[calc(100vh-80px)] overflow-hidden">
       <div className="w-max h-full flex transition-all ease-in-out duration-1000">
-        {slides.map(slide=>(
-          <div className={'${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row'} key={slide.id}>
+        {slides.map((slide)=>(
+          <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+           key={slide.id}
+           >
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 xl:w-1/2">
-              <h2>{slide.description}</h2>
-              <h1>{slide.title}</h1>
-              <Link href={slide.url}><button>SHOP NOW</button></Link>
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:5xl">{slide.description}</h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+              <Link href={slide.url}>
+                <button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button>
+              </Link>
             </div>
             {/* IMAGE CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 relative">
+            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
               <Image src={slide.img} alt="" fill sizes="100%" className="object-cover"/>
             </div>
           </div>
         ))}
       </div>
+      {
+        slides.map(slide=>(
+          <div className={`w-3 h-3 `} key={slide.id}></div>
+        ))
+      }
     </div>
   );
 };
