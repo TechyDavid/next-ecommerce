57722edd71cfab6b3995e3868c970fcbd7b166b0
TechyDavid <drufus015@gmail.com>
2024-10-30 19:21:26 +0100

    Long time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Homepage = () => {
                 <h1 className="text-2xl">Featured Products</h1>
                 <ProductList/>
             </div>
-            <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+            <div className="mt-24">
                 <h1 className="text-2xl">Catergories</h1>
                 <CategoryList/>
             </div>
