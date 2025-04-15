c87eece98ce2909a2d899253823fc4a570ba02df
Rufus <drufus015@gmail.com>
2025-04-15 12:56:38 +0100

    Update and rename chatbot.tsx to Chatbot.tsx

---
+++ src/components/Chatbot.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <div className="">
+          <iframe height="430" width="350" src="https://bot.dialogflow.com/286cffad-d80e-4ab1-bfe9-3d80638f716f"></iframe>
+        </div>
+    )
+}
+export default Footer
