0fda85e23815ab234423da7b85a4937aa757291f
Rufus <drufus015@gmail.com>
2025-04-15 13:44:41 +0100

    fin Update Chatbot.tsx

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,15 +1,15 @@
-// const Chatbot = () => {
-//     return (
-        
-//          <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-//         <df-messenger
-//           intent="WELCOME"
-//           chat-title="Faqbot"
-//           agent-id="286cffad-d80e-4ab1-bfe9-3d80638f716f"
-//           language-code="en"
-//     >
-//         </df-messenger>
-      
-//     )
-// }
-// export default Chatbot;
+const Chatbot = () => {
+    return (
+        <div>
+         <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+        <df-messenger
+          intent="WELCOME"
+          chat-title="Faqbot"
+          agent-id="286cffad-d80e-4ab1-bfe9-3d80638f716f"
+          language-code="en"
+    >
+        </df-messenger>
+       </div>
+    )
+}
+export default Chatbot;
