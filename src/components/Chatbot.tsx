b9962563910f45e52dfe1c787d21d8c8eab38b75
Rufus <drufus015@gmail.com>
2025-04-15 13:51:22 +0100

    finaalllyy Update Chatbot.tsx

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,14 +1,13 @@
 const Chatbot = () => {
     return (
         <div>
-         <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-        <df-messenger
-          intent="WELCOME"
-          chat-title="Faqbot"
-          agent-id="286cffad-d80e-4ab1-bfe9-3d80638f716f"
-          language-code="en"
-    >
-        </df-messenger>
+      <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  chat-icon="https:&#x2F;&#x2F;mtu.edu.ng&#x2F;images&#x2F;home&#x2F;logo.svg"
+  chat-title="Faqbot"
+  agent-id="286cffad-d80e-4ab1-bfe9-3d80638f716f"
+  language-code="en"
+></df-messenger>
        </div>
     )
 }
