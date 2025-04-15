5a2d077b948f54f56ec03c51a161607ca1d7bb8e
Rufus <drufus015@gmail.com>
2025-04-15 12:52:48 +0100

    1Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Chatbot from "@/components/chatbot";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
+        </chatbot>
         <Footer/>
       </body>
     </html>
