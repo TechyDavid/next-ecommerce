5a218f91b7a482180d6f95c91ef60d2f036050df
Rufus <drufus015@gmail.com>
2025-04-15 13:16:22 +0100

    Update Chatbot.tsx

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,8 +1,8 @@
-const Footer = () => {
+const Chatbot = () => {
     return (
         <div className="">
           <iframe height="430" width="350" src="https://bot.dialogflow.com/286cffad-d80e-4ab1-bfe9-3d80638f716f"></iframe>
         </div>
     )
 }
-export default Footer
+export default Chatbot
