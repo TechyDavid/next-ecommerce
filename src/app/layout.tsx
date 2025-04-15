b6fbc06f65d2ff50bba3f039f1eba239b8aadb8b
Rufus <drufus015@gmail.com>
2025-04-15 13:35:54 +0100

    final Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
         <Chatbot/>
         <Footer/>
       </body>
+      <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="Faqbot"
+  agent-id="286cffad-d80e-4ab1-bfe9-3d80638f716f"
+  language-code="en"
+></df-messenger>
     </html>
   );
 }
